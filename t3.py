72f6715c8fc7caa8f4bdfaa4e061ae51dab85f90
sujinze0415-ai <sujinze0415@gmail.com>
2025-11-06 11:42:33 -0800

    Update content and interactions in t3.py

--- t3.py
+++ t3.py
@@ -7,13 +7,17 @@ st.progress(50)
 
 st.header("This is my first step")
 st.image("https://i.17173cdn.com/2fhnvk/YWxqaGBf/cms3/adxJDQbsyhluvuF.png!a-3-480x.png")
-st.latex("真不错")
-st.video("https://www.youtube.com/watch?v=UJAtXLfR0n8", autoplay=True, muted=True, loop=True) 
+st.latex("NANBEILVDOU")
+st.video("https://www.youtube.com/watch?v=UJAtXLfR0n8"）
+st.latex("HAJIMI WAKE UP")
+st.video("https://www.youtube.com/watch?v=IEWEiUEPmiM")
+st.latex("TIAO LOU JI")
+st.video("https://www.youtube.com/watch?v=dztust9BNqE")
 
-st.checkbox("This is a checkbox")
-st.button("This is a button")
-st.selectbox("This is a selectbox", options=["A", "B", "C"])
-st.multiselect("This is a multiselect", options=["A", "B", "C"])
+st.checkbox("IF YOU LIKE HAJIMI")
+st.button("SAY HI!")
+st.selectbox("RATE HAJIMI", options=["1", "5", "10"])
+st.multiselect("WHICH HAJIMI SOUND YOU LIKE THE MOST", options=["A", "B", "C"])
 st.slider("This is a slider", min_value=0, max_value=100, value=50)
 st.select_slider("This is a select slider", options=["A", "B", "C"])
 
